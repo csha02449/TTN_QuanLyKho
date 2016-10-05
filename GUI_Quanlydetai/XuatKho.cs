c86abdc7c07f9dd3fe58dbb2d10db2483d13e7b8
cuopemveday <nguyenhuuhuy1581995@gmail.com>
2016-10-06 00:18:29 +0700

    edit xuat kho

--- GUI_Quanlydetai/XuatKho.cs
+++ GUI_Quanlydetai/XuatKho.cs
@@ -308,12 +308,26 @@ namespace GUI_Quanlydetai
         //dang sua chua xong
         private void txtMaKho_Click(object sender, EventArgs e)
         {
-
+            if (txtTenKho.Text != " ")
+            {
+                con=DAO_KetNoiDB.OpenConnect();
+                string s1 = txtTenKho.Text;
+                string s2 = SqlHelper.ExecuteScalar(con,"hienthitenthanhmakh",s1).ToString();
+                txtMaKho.Text = s2;
+            }
+            else txtMaKho.Text = " ";
         }
 
         private void txtMaHH_Click(object sender, EventArgs e)
         {
-
+            if (txtTenHH.Text != " ")
+            {
+                con = DAO_KetNoiDB.OpenConnect();
+                string s1 = txtTenHH.Text;
+                string s2 = SqlHelper.ExecuteScalar(con, "hienthitenthanhmahangho", s1).ToString();
+                txtMaHH.Text = s2;
+            }
+            else txtMaKho.Text = " ";
         }
 
         private void txtMaKH_Click(object sender, EventArgs e)
